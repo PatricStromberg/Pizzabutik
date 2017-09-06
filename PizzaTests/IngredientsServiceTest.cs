0c716058d65d8b4735b1505eb23862e8ac6a5e3f
patricstromberg <stromberg.patric@hotmail.com>
2017-09-06 10:35:31 +0200

    Skapade ett test för ingredientservice

---
+++ PizzaTests/IngredientsServiceTest.cs
@@ -0,0 +1,38 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NostalgiPizza.Data;
+using Xunit;
+
+namespace PizzaTests
+{
+    public class IngredientsServiceTest
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public IngredientsServiceTest()
+        {
+            var efServiceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            var services = new ServiceCollection();
+
+            services.AddDbContext<ApplicationDbContext>(b => b
+                .UseInMemoryDatabase("Pizzadatabas")
+                .UseInternalServiceProvider(efServiceProvider));
+            services.AddTransient<IngredientsService>();
+
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        [Fact]
+        public void All_Are_Sorted()
+        {
+            var _ingredients = _serviceProvider.GetService<IngredientsService>();
+            var ings = _ingredients.All();
+
+            Assert.Equal(ings.Count, 0);
+        }
+    }
+}
